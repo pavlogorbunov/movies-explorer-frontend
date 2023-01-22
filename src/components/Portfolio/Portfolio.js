43f95919d7b803ad693c341f3146aa97de946f04
pavlogorbunov <pavlogorbunov@yandex.ru>
2023-01-22 20:52:15 +0300

    jsx 1.2

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,11 +6,15 @@ function Portfolio() {
             <h3 className="portfolio__header">Портфолио</h3>
             <ul className="portfolio__list">
                 <li className="portfolio__listitem">
-                    <a href="https://xn----9sbmlc4adegsjcp8f.xn--p1ai/" target="_blank" rel="noreferrer" className="portfolio__listitem-text">Интернет-магазин на WordPress</a>
+                    <a href="https://pavlogorbunov.github.io/how-to-learn-pavlogorbunov/" target="_blank" rel="noreferrer" className="portfolio__listitem-text">Статичный сайт</a>
                     <p className="portfolio__listitem-arrow">↗</p>
                 </li>
                 <li className="portfolio__listitem">
-                    <a href="https://paulogorbunov.nomoredomains.club/" target="_blank" rel="noreferrer" className="portfolio__listitem-text">Одностраничное React-приложение</a>
+                    <a href="https://pavlogorbunov.github.io/russian-travel-pavlogorbunov/" target="_blank" rel="noreferrer" className="portfolio__listitem-text">Адаптивный сайт</a>
+                    <p className="portfolio__listitem-arrow">↗</p>
+                </li>
+                <li className="portfolio__listitem">
+                    <a href="https://paulogorbunov.nomoredomains.club/" target="_blank" rel="noreferrer" className="portfolio__listitem-text">Одностраничное приложение</a>
                     <p className="portfolio__listitem-arrow">↗</p>
                 </li>
             </ul>
